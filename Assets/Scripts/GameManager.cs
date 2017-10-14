ad158bf266e27daaece071b4781cf910efd33f09
Shamsco <a_shamiss@hotmail.com>
2017-10-13 23:59:05 -0600

    The Beginning
    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+    private int level = 3;
+
+    void Awake()
+    {
+
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        //Call the SetupScene function of the BoardManager script, pass it current level number.
+        boardScript.SetupScene(level);
+
+    }
+
+
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+}
