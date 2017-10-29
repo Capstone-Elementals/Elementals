f7702c819cb991c8540879c766de1dcdc60e8905
Shamsco <a_shamiss@hotmail.com>
2017-10-28 19:34:53 -0600

    Animation
    Trying to animate. Failure so far

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,17 +7,15 @@ public class Player : PhysicsObject
 
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
-    
+	private Animator animator;
     private SpriteRenderer spriteRenderer;
-    //private Animator animator;
 
     // Use this for initialization
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
-
     protected override void ComputeVelocity()
     {
         Vector2 move = Vector2.zero;
@@ -25,19 +23,20 @@ public class Player : PhysicsObject
         move.x = Input.GetAxis("Horizontal");
 
 		move.y = Input.GetAxis ("Vertical");
-
+	
 		if (move.y > 0 && grounded) {
 			velocity.y = jumpTakeOffSpeed;
 		} else if (move.y < 0) {
 			velocity.y = -jumpTakeOffSpeed * 2f;
 		}
         bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        if (flipSprite)
+       if (flipSprite)
         {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
+         
+			spriteRenderer.flipX = !spriteRenderer.flipX;
         }
 
-        //animator.SetBool("grounded", grounded);
+       // animator.SetBool("grounded", grounded);
         //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
         targetVelocity = move * maxSpeed;
