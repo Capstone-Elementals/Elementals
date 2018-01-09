519f0259291ea1fcf33ba84f17afa48a3cb75310
joshleidl <joshleidl@gmail.com>
2018-01-09 12:12:28 -0600

    Begin refactor

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -13,10 +13,14 @@ public abstract class Bullet : MonoBehaviour {
 	Vector2 bulletVelocity = Vector2.zero;
 
 	// Use this for initialization
+	/**
+	 * As of right now, bullets get the aim direction upon spawn
+	 */
 	public void Start () {
 		bulletVelocity.x = UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxis ("HorizontalShoot");
 		bulletVelocity.y = UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxis ("VerticalShoot");
 		Rigidbody2D rb2d = this.GetComponent<Rigidbody2D> ();
+		//Add a force equal to speed times the unit vector of the aiming joystick
 		rb2d.AddForce ((bulletVelocity / bulletVelocity.magnitude) * speed);
 	}
 	
@@ -28,6 +32,7 @@ public abstract class Bullet : MonoBehaviour {
 		timeout--;
 	}
 
+	// Most bullets will call this upon collision
 	public void OnCollisionEnter2D (Collision2D col) {
 		Destroy(this.gameObject);
 	}
