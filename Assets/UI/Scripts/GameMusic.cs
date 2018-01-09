aaea7e7756130ada2f20d24dd48b2b4ad53ef128
Alhwirif <oozz.2014@gmail.com>
2018-01-09 12:11:56 -0600

    Ui
    UI

---
+++ Assets/UI/Scripts/GameMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/UI/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Loading Level" + name);
+		Application.LoadLevel (name);
+	}
+
+
+}
