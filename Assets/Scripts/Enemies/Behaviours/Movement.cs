da34c63dda1808cd0dd5ed1c486a7831bcb93a9c
joshleidl <joshleidl@gmail.com>
2018-01-21 14:23:13 -0600

    Enemy script for turning around at walls works. Currently raycasts are drawn for debugging purposes. Wall collision relies on walls not having rigidbodies!

--- Assets/Scripts/Enemies/Behaviours/Movement.cs
+++ Assets/Scripts/Enemies/Behaviours/Movement.cs
@@ -8,30 +8,35 @@ public class Movement : MonoBehaviour {
 	public enum movementDirection {left, right}
 
 	public movementDirection direction = movementDirection.right;
-	public float speed = .5f;
+	public float speed = .25f;
 
 	private Rigidbody2D	rb2d;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = (Rigidbody2D) GetComponent<Rigidbody2D> ();
-		updateDirection ();
 	}
 
-	void updateDirection() {
-		if (direction == movementDirection.left) {
-			rb2d.AddForce (new Vector2(-speed, 0));
+	void FixedUpdate() {
+		Vector2 currentVelocity = rb2d.velocity;
+
+		if (direction == movementDirection.right) {
+			rb2d.velocity = new Vector2 (speed, currentVelocity.y);
 		} else {
-			rb2d.AddForce (new Vector2 (speed, 0));
+			rb2d.velocity = new Vector2 (-speed, currentVelocity.y);
 		}
 	}
 	
 	public void setDirection(movementDirection newDirection) {
 		if (direction != newDirection) {
 			direction = newDirection;
-			rb2d.velocity = Vector2.zero;
-			rb2d.angularVelocity = 0f;
-			updateDirection ();
 		}
+	}
+
+	public void turnAround() {
+		if (direction == movementDirection.right)
+			setDirection (movementDirection.left);
+		else
+			setDirection (movementDirection.right);
 	}
 }
---
+++ Assets/Scripts/Enemies/Behaviours/TurnAroundAtWall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Movement))]
+public class TurnAroundAtWall : MonoBehaviour {
+
+	private Movement movement;
+	private Rigidbody2D rb2d;
+	private BoxCollider2D box2d;
+
+	void Start() {
+		movement = (Movement)GetComponent<Movement> ();
+		rb2d = (Rigidbody2D)GetComponent<Rigidbody2D> ();
+		box2d = (BoxCollider2D)GetComponent<BoxCollider2D> ();
+	}
+
+	//Coding plan: raycast a few pixels in the direction of movement
+	// if anything but a bullet or player is hit, turn around
+	void FixedUpdate () {
+
+		bool aimLeft = false;
+		RaycastHit2D rh2d;
+		Vector2 direction = Vector2.right;
+
+		//Calculate the horizontal offset the raycast should use
+		float offset = (box2d.size.x / 2f) * 1.03f;
+
+		//If moving left, offset must be 
+		if (movement.direction == Movement.movementDirection.left) {
+			aimLeft = true;
+			offset = -offset;
+			direction = Vector2.left;
+		}
+
+		Vector2 origin = new Vector2 (rb2d.transform.position.x + offset, rb2d.transform.position.y);
+
+		//Raycast out 5 pixels
+		rh2d = Physics2D.Raycast (origin, direction, 0.03f);
+		Debug.DrawLine ((Vector3)origin, (Vector3)(origin + direction * 0.03f));
+
+		//Returns false if nothing was hit
+		if (rh2d) {
+			//If hit object is another enemy or a wall turn around
+			if (rh2d.rigidbody == null || rh2d.rigidbody.gameObject.name.Contains ("Enemy")) {
+				movement.turnAround ();
+			}
+		}
+	}
+}
