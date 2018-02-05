03241673a9b13d380adae3201cee5ab8af1ae94e
Shamsco <a_shamiss@hotmail.com>
2018-02-04 19:56:10 -0600

    Prototype
    New Player Controller, works decently for now

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,19 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-
-	protected Rigidbody2D rb2d;
-
-	void OnEnable()
+	public float maxpeed = 10f;
+	bool facingRight = true;
+	private Rigidbody2D rb2d;
+	Animator anim;
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700;
+	void Start()
 	{
-		rb2d = GetComponent<Rigidbody2D>();
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	void FixedUpdate()
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+		anim.SetFloat("vSpeed", rb2d.velocity.y);
+		Debug.Log (grounded);
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		rb2d.velocity = new Vector2 (move * maxpeed, rb2d.velocity.y); 
+		if (move > 0 && !facingRight) {
+			Flip ();		}
+		else if(move < 0 && facingRight)
+		{
+			Flip();
+		}
+
+
 	}
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-		var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-		transform.Translate(x, y, 0);
+		if(grounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			anim.SetBool ("Ground", false);
+			rb2d.AddForce (new Vector2 (0, jumpForce));
+		}
+	}
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
