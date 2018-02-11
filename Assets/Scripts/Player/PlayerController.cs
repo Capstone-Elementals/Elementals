62c649692d9068da5ac2c4ffa907bbb57d00729a
Shamsco <a_shamiss@hotmail.com>
2018-02-11 13:49:24 -0600

    Prep work
    Preparing to add tiles

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,6 +13,39 @@ public class PlayerController : MonoBehaviour
 	float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
 	public float jumpForce = 700;
+	private float essence = 0;
+	private Armor playerArmor;
+	private Boot playerBoot;
+	private Weapon playerWeapon1;
+	private Weapon playerWeapon2;
+	private bool jumpPending;
+	public void setArmor(Armor armor){
+		this.playerArmor = armor;
+	}
+	public void setBoot(Boot boot){
+		this.playerBoot = boot;
+	}
+	public void setWeapon1(Weapon weapon1){
+		this.playerWeapon1 = weapon1;
+	}
+	public void setWeapon2(Weapon weapon2){
+		this.playerWeapon2 = weapon2;
+	}
+	public Armor getArmor(){
+		return playerArmor;
+	}
+	public Boot getBoot(){
+		return playerBoot;
+	}
+	public Weapon getWeapon1(){
+		return playerWeapon1;
+	}
+	public Weapon getWeapon2(){
+		return playerWeapon2;
+	}
+	public void jump() {
+		jumpPending = true;
+	}
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
@@ -24,7 +57,7 @@ public class PlayerController : MonoBehaviour
 		anim.SetBool ("Ground", grounded);
 		anim.SetFloat("vSpeed", rb2d.velocity.y);
 		Debug.Log (grounded);
-		float move = Input.GetAxis ("Horizontal");
+		float move = UnityStandardAssets.CrossPlatformInput.CrossPlatformInputManager.GetAxis ("HorizontalMove") * 0.5f;
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 		rb2d.velocity = new Vector2 (move * maxpeed, rb2d.velocity.y); 
 		if (move > 0 && !facingRight) {
