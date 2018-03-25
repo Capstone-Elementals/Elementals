6af4b0d906a498f5780c71c13c6a9415b4e1a115
Shamsco <a_shamiss@hotmail.com>
2018-03-25 15:41:33 -0600

    NewGame
    Newgame deletes old save now

--- Assets/Scripts/GameStart/NewGame.cs
+++ Assets/Scripts/GameStart/NewGame.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 public class NewGame : MonoBehaviour{
 
 	public GameObject inventory;
 	// Use this for initialization
 	void Awake()
 	{
+		if (File.Exists (Application.persistentDataPath + "/inventory.data")) {
+			File.Delete (Application.persistentDataPath + "/inventory.data");
+		}
 		if (Inventory.instance == null)
 		{
 			Instantiate(inventory);
