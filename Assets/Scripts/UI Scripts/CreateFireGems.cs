d38b39ec1d96a9374e2f52b917ce76d964466cbe
Shamsco <a_shamiss@hotmail.com>
2018-03-28 00:46:27 -0600

    Faris Stuff
    Farises stuff. Hopefully that works

--- Assets/Scripts/UI Scripts/CreateFireGems.cs
+++ Assets/Scripts/UI Scripts/CreateFireGems.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMusic : MonoBehaviour {
+public class CreateFireGems : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad (gameObject);
+		
 	}
 	
-
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
