ba8b433e2cc31e66ad06d3f632004ad2902c58d5
Alhwirif <oozz.2014@gmail.com>
2018-04-01 18:46:15 -0600

    Revert "Merge branch 'master' of https://github.com/Capstone-Elementals/Elementals"
    This reverts commit 7dd889164b41c5f4b876c429624c2ab6b2449a1a, reversing
    changes made to 640d8daf5b15d308caeae320394861b1e9d6d8f5.

--- Assets/Scripts/UI Scripts/UIArmory.cs
+++ Assets/Scripts/UI Scripts/UIArmory.cs
@@ -4,41 +4,11 @@ using UnityEngine;
 
 public class UIArmory : MonoBehaviour {
 
-	private GameObject parent; 
-	public GameObject gem;
-	void Awake()
-	{
-
-		for (int i = 0; i < Inventory.inventory.Count; i++){
-
-			Gem temp = Inventory.inventory [i];
-
-			parent = GameObject.Find ("Slot " + i);
-			Transform tempChild = parent.transform.GetChild (0);
-			Destroy (tempChild.gameObject);
-			switch (temp.getElement ()) {
-			case 'F':
-				gem.GetComponent<UnityEngine.UI.Image> ().color = Color.red;
-				break;
-			case 'W':
-				gem.GetComponent<UnityEngine.UI.Image> ().color =  Color.blue;
-				break;
-			case 'A':
-				gem.GetComponent<UnityEngine.UI.Image> ().color = Color.cyan;
-				break;
-			case 'E':
-				gem.GetComponent<UnityEngine.UI.Image> ().color = new Color(0.855f,0.388f,0.086f,1.0f);
-				break;
-			default:
-				break;
-			}
-			gem.GetComponentInChildren<UnityEngine.UI.Text> ().text = temp.getGrade().ToString();
-			GameObject toinstance = Instantiate (gem,parent.transform,false) as GameObject;
-
-		}
-
+	// Use this for initialization
+	void Start () {
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		
