f89c32e827e0289147f1f780fa4085d315584a60
Shamsco <a_shamiss@hotmail.com>
2018-04-02 17:14:46 -0600

    UI Fixes

--- Assets/Scripts/UI Scripts/WorldUI.cs
+++ Assets/Scripts/UI Scripts/WorldUI.cs
@@ -25,8 +25,30 @@ public class WorldUI : MonoBehaviour {
 			gem1.GetComponentInChildren<UnityEngine.UI.Text> ().text = player.GetComponent<PlayerController> ().equippedWeapon.getGem1 ().getGrade ().ToString ();
 			gem2.GetComponentInChildren<UnityEngine.UI.Text> ().text = player.GetComponent<PlayerController> ().equippedWeapon.getGem2 ().getGrade ().ToString ();
 			gem3.GetComponentInChildren<UnityEngine.UI.Text> ().text = player.GetComponent<PlayerController> ().equippedWeapon.getGem3 ().getGrade ().ToString ();
+			gem1.GetComponent<UnityEngine.UI.Image> ().color = getGemColor (player.GetComponent<PlayerController> ().equippedWeapon.getGem1 ());
+			gem2.GetComponent<UnityEngine.UI.Image> ().color = getGemColor (player.GetComponent<PlayerController> ().equippedWeapon.getGem2 ());
+			gem3.GetComponent<UnityEngine.UI.Image> ().color = getGemColor (player.GetComponent<PlayerController> ().equippedWeapon.getGem3 ());
 			essence.GetComponentInChildren<UnityEngine.UI.Text> ().text = Inventory.essence.ToString ();
 		}
 	}
-
+	Color getGemColor(Gem gem)
+	{
+		switch (gem.getElement()) {
+		case 'F':
+			return Color.red;
+			break;
+		case 'W':
+			return Color.blue;
+			break;
+		case 'A':
+			return Color.cyan;
+			break;
+		case 'E':
+			return Inventory.brown;
+			break;
+		default:
+			break;
+		}
+		return Color.white;
+	}
 }
