dfecbc1dc46471abd5b1ced242fcf3e1fbc6e662
Shamsco <a_shamiss@hotmail.com>
2018-04-01 19:38:56 -0600

    Armor work

--- Assets/Scripts/Player Gear/Armor.cs
+++ Assets/Scripts/Player Gear/Armor.cs
@@ -10,25 +10,19 @@ using UnityEngine;
 public class Armor 
 {
 	private Gem gem1;
-	private Gem gem2;
-	private Gem gem3;
 	private float defense;
 	private float bonusDefense;
 	private float totalDefense;
 	public Armor()
 	{
 		gem1 = new Gem();
-		gem2 = new Gem();
-		gem3 = new Gem();
 		defense = 0;
 		bonusDefense = 0;
 		totalDefense = 0;
 	}
-	public Armor(Gem gem1, Gem gem2, Gem gem3, float defense)
+	public Armor(Gem gem1,  float defense)
 	{
 		this.gem1 = gem1;
-		this.gem2 = gem2;
-		this.gem3 = gem3;
 		this.defense = defense;
 		calculateBonusDefense ();
 		calculateTotalDefense ();
@@ -37,25 +31,9 @@ public class Armor
 	{
 		this.gem1 = gem;
 	}
-	public void setGem2(Gem gem)
-	{
-		this.gem2 = gem;
-	}
-	public void setGem3(Gem gem)
-	{
-		this.gem3 = gem;
-	}
 	public Gem getGem1()
 	{
 		return this.gem1;
-	}
-	public Gem getGem2()
-	{
-		return this.gem2;
-	}
-	public Gem getGem3()
-	{
-		return this.gem3;
 	}
 	public void setDefense(float defense)
 	{
@@ -84,7 +62,7 @@ public class Armor
 	public void calculateBonusDefense()
 	{
 		float temp;
-		temp = (gem1.getBonus () * defense) + (gem2.getBonus () * defense) + (gem3.getBonus () * defense);
+		temp = (gem1.getBonus () * defense);
 		setBonusDefense (temp);
 	}
 	public void calculateTotalDefense()
